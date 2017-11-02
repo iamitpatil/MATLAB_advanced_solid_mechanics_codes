b7aa46e3ea0f540642fafa1fccb537f4966a56bc
Amit Patil <amitpatil@iitkgp.ac.in>
2017-11-02 12:09:17 +0530

    Updated Surface plots with moment plots.

--- Mindins Plate Theory/main.m
+++ Mindins Plate Theory/main.m
@@ -13,10 +13,14 @@ load=1;
 w=zeros(31,31);
 theta_x=zeros(31,31);
 theta_y=zeros(31,31);
+M_x=zeros(31,31);
+M_y=zeros(31,31);
+M_xy=zeros(31,31);
 i=1; j=1;
 for x=0:0.001:a
     for y=0:0.001:b
         resp=[0;0;0];
+        diff=[0;0;0];
        for m=1:10
            for n=1:10
             alpha=m*pi/a;
@@ -25,8 +29,12 @@ for x=0:0.001:a
             P=[cal_P( m, n, load,a,b); 0; 0];
             coeff=inv(K)*P;
             resp=resp+coeff.*[sin(alpha*x)*sin(beta*y); cos(alpha*x)*sin(beta*y); sin(alpha*x)*cos(beta*y)];
+            diff=diff+[-alpha*coeff(2,1)*sin(alpha*x)*sin(beta*y); -beta*coeff(3,1)*sin(alpha*x)*sin(beta*y); (coeff(2,1)*beta+coeff(3,1)*alpha)*cos(alpha*x)*cos(beta*y)];
            end
        end
+       M_x(j,i)=(c11*diff(1,1)+c12*diff(2,1))*(h^3)/12;
+       M_y(j,i)=(c11*diff(2,1)+c12*diff(1,1))*(h^3)/12;
+       M_xy(j,i)=c44*diff(3,1)*(h^3)/12;
        w(j,i)=resp(1,1);
        theta_x(j,i)=resp(2,1);
        theta_y(j,i)=resp(3,1);
@@ -35,30 +43,68 @@ for x=0:0.001:a
     j=1;
     i=i+1;
 end
-subplot(1,3,1);
-[xx,yy]=meshgrid([0:0.001:a],[0:0.001:b]);
+x=[0:0.001:a];
+y=[0:0.001:b];
+subplot(2,3,1);
+[xx,yy]=meshgrid(x,y);
 surf(xx,yy,w);
+colormap jet
 xlabel('x in m');
 ylabel('y in m');
 zlabel('w (m)');
+title('w with (x,y)');
 % shading interp
 grid on
-subplot(1,3,2);
-[xx,yy]=meshgrid([0:0.001:a],[0:0.001:b]);
+subplot(2,3,2);
+[xx,yy]=meshgrid(x,y);
 z=h/2;
 surf(xx,yy,theta_x*z);
+colormap jet
 xlabel('x in m');
 ylabel('y in m');
 zlabel('u at top surface');
+title('u at top surface with (x,y)');
 % shading interp
 grid on
-subplot(1,3,3);
-[xx,yy]=meshgrid([0:0.001:a],[0:0.001:b]);
+subplot(2,3,3);
+[xx,yy]=meshgrid(x,y);
 z=h/2;
 surf(xx,yy,theta_y*z);
+colormap jet
 xlabel('x in m');
 ylabel('y in m');
 zlabel('v at top surface');
+title('v at top surface with (x,y)');
+% shading interp
+grid on
+subplot(2,3,4);
+[xx,yy]=meshgrid(x,y);
+surf(xx,yy,M_x);
+colormap jet
+xlabel('x in m');
+ylabel('y in m');
+zlabel('M_x');
+title('M_x with (x,y)');
+% shading interp
+grid on
+subplot(2,3,5);
+[xx,yy]=meshgrid(x,y);
+surf(xx,yy,M_y);
+colormap jet
+xlabel('x in m');
+ylabel('y in m');
+zlabel('M_y');
+title('M_y with (x,y)');
+% shading interp
+grid on
+subplot(2,3,6);
+[xx,yy]=meshgrid(x,y);
+surf(xx,yy,M_xy);
+colormap jet
+xlabel('x in m');
+ylabel('y in m');
+zlabel('M_xy');
+title('M_xy with (x,y)');
 % shading interp
 grid on
 toc;
